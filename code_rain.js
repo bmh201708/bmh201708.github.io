169c4966d4b4ceea59855120d648fe7cfd08a8b0
bmh201708 <1825251317@qq.com>
2020-03-01 14:13:25 +0800

    Site updated: 2020-03-01 14:13:23

--- code_rain.js
+++ code_rain.js
@@ -55,6 +55,3 @@ window.onload = function(){
     draw();
     setInterval(draw,35);
 };
-————————————————
-版权声明：本文为CSDN博主「秦时明月之君临天下」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/weixin_41287260/article/details/103050877
